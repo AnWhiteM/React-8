6a5880c27f64874539cab85cdac77d41b2c92e88
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2024-03-15 13:18:36 +0200

    6th commit

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,33 +1,48 @@
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../redux/auth/operations';
 import css from './LoginForm.module.css'
+import { useId } from "react";
+import { Formik, Form, Field} from 'formik';
+import toast from "react-hot-toast";
+import * as Yup from 'yup';
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
-  };
+  const userEmail = useId();
+    const userPassword = useId();
+
+    const validationScheme = Yup.object().shape({
+        email: Yup.string().matches(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm).min(3, 'Too short!').max(50, 'Too long!'),
+        password: Yup.string().min(3, 'Too short!').max(30, 'Too long!')
+    });
+
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off" className={css.loginForm}>
-      <label>
-        Email:
-        <input type="email" name="email" className={css.input}/>
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" className={css.input}/>
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    
+      <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={validationScheme}
+            onSubmit={(values, actions) => { 
+              dispatch(logIn({ ...values }))
+                  .unwrap()
+                  .then(() => {})
+                  .catch(() => {
+                      toast.error('Email or password is incorrect, try again')
+                  })
+              actions.resetForm();
+          }}
+            
+        >
+            <Form className={css.loginForm}>
+                <label htmlFor={userEmail}>Email:</label>
+                <Field type="email" name="email" id={userEmail} className={css.input}/>
+
+                <label htmlFor={userPassword}>Password:</label>
+                <Field type="password" name="password" id={userPassword} className={css.input}/>
+
+                <button type="submit">Log In</button>
+          </Form>
+        </Formik>
   );
 };
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,38 +1,47 @@
 import { useDispatch } from 'react-redux';
 import { register } from '../../redux/auth/operations';
 import css from './RegisterForm.module.css'
+import { Formik, Form, Field} from 'formik';
+import { v4 as uuidv4 } from 'uuid';
+import { useId } from "react";
+import * as Yup from 'yup';
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
+  const userName = useId();
+    const userEmail = useId();
+    const userPassword = useId();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
-  };
+    const validationScheme = Yup.object().shape({
+      email: Yup.string().matches(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm).min(3, 'Too short!').max(50, 'Too long!'),
+      password: Yup.string().min(3, 'Too short!').max(30, 'Too long!')
+  });
+
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off" className={css.regForm}>
-      <label>
-        Username: 
-        <input type="text" name="name" className={css.input}/>
-      </label>
-      <label>
-        Email:
-        <input type="email" name="email" className={css.input}/>
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" className={css.input}/>
-      </label>
-      <button type="submit">Register</button>
-    </form>
+
+    <Formik
+      initialValues={{ name: '', email: '', password: '' }}
+      validationSchema={validationScheme}
+      onSubmit={(values, actions) => { 
+          const id = uuidv4();
+          dispatch(register({id: id, ...values }));
+          actions.resetForm();
+    }}
+      >
+        <Form className={css.regForm}>
+        <label htmlFor={userName}> Username:</label>
+        <Field type="text" name="name" id={userName} className={css.input}/>
+
+        <label htmlFor={userEmail}>Email:</label>
+        <Field type="email" name="email" id={userEmail} className={css.input}/>
+
+        <label htmlFor={userPassword}>Password:</label>
+        <Field type="password" name="password" id={userPassword} className={css.input}/>
+
+        <button type="submit">Register</button>
+      
+        </Form>
+    </Formik>
   );
 };
